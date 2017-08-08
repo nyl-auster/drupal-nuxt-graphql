7bfe115f0cab653f566da1aacebd44a4097e6beb
yann <yann.boisselier@gmail.com>
2017-08-08 18:13:03 +0200

    graphql bitch

--- pages/blog.vue
+++ pages/blog.vue
@@ -2,8 +2,9 @@
   <section class="container">
     <h1 class="title is-1">Blog</h1>
   
-    <div class="post" v-for="post in posts">
+    <div v-if="nodeQuery" class="post" v-for="post in nodeQuery.entities">
       <h3 class="title is-3">{{ post.entityLabel }}</h3>
+      <div class="body">{{ post.body }} </div>
       <hr />
     </div>
   
@@ -14,7 +15,7 @@
 import allPosts from '~/apollo/queries/allPosts'
 export default {
   apollo: {
-    posts: {
+    nodeQuery: {
       query: allPosts
     }
   }
